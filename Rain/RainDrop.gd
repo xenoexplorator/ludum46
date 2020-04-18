c777dc5ebb207af86e93cfeac31b0ddbb3f73fb0
xenoexplorator <cedric.pinard@gmail.com>
2020-04-18 08:44:38 -0400

    Ajout d'une logique de base de spawn de pluie

--- Rain/RainDrop.gd
+++ Rain/RainDrop.gd
@@ -7,8 +7,11 @@ var _velocity : Vector2
 func _ready() -> void:
 	angle = deg2rad(angle)
 	_velocity = speed * Vector2(sin(angle), cos(angle))
-	print(_velocity)
 
 
 func _physics_process(delta: float) -> void:
 	move_and_collide(delta * _velocity)
+
+
+func _on_screen_exited() -> void:
+	queue_free()
