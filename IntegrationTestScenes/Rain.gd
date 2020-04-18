4ba730f77922684e76631019681cfe4792be971a
xenoexplorator <cedric.pinard@gmail.com>
2020-04-18 10:32:25 -0400

    Ìntégration du sprite de pluie et 2 nouveaux motifs

--- IntegrationTestScenes/Rain.gd
+++ IntegrationTestScenes/Rain.gd
@@ -1,6 +1,6 @@
 extends Node2D
 
-enum Pattern { RANDOM, WAVE }
+enum Pattern { RANDOM, WAVE, RAYS, BEAMS }
 const SPAWN_THRESHOLD := 10.0
 const RainDrop = preload("res://Rain/RainDrop.tscn")
 export (Pattern) var pattern := Pattern.RANDOM
@@ -21,6 +21,12 @@ func _process(delta: float) -> void:
 				spawn_random()
 			Pattern.WAVE:
 				spawn_wave()
+			Pattern.RAYS:
+				spawn_rays()
+			Pattern.BEAMS:
+				spawn_beams()
+	$Control/VBoxContainer/Count.text = "Drops# %d" % $Container.get_child_count()
+
 
 func spawn_random() -> void:
 	var drop = RainDrop.instance()
@@ -36,8 +42,29 @@ func spawn_wave() -> void:
 	var drop = RainDrop.instance()
 	var x = 2530 - 100 * abs(pattern_counter - 25)
 	pattern_counter = (pattern_counter + 1) % 50
-	drop.position = Vector2(x, 10)
+	drop.position = Vector2(x, 20)
 	$Container.add_child(drop)
+
+
+func spawn_rays() -> void:
+	for th in range(-75, 76, 15):
+		var drop = RainDrop.instance()
+		drop.position = Vector2(1280, 30)
+		drop.angle = th
+		drop.speed = 100
+		$Container.add_child(drop)
+
+
+func spawn_beams() -> void:
+	var origin = Vector2(rand_range(100, 2480), rand_range(10, 60))
+	var target = Vector2(rand_range(100, 2480), rand_range(1360, 1460))
+	var angle = rad2deg((target - origin).angle()) - 90
+	for v in range(60, 220, 10):
+		var drop = RainDrop.instance()
+		drop.position = origin
+		drop.angle = angle
+		drop.speed = v
+		$Container.add_child(drop)
 
 
 func change_pattern(pat: int) -> void:
