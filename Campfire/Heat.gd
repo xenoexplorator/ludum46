7602ee0774f5d32ca4df17980b3ee1b4ed96d531
Ellias, Mélanie <Melanie.Ellias@ia.ca>
2020-04-18 18:47:45 -0400

    feu et bar fonctionnels

--- Campfire/Heat.gd
+++ Campfire/Heat.gd
@@ -1,15 +1,24 @@
 extends Area2D
 
+signal heat_changed(heat)
+
+var heat = 0
+var max_heat = 100
 
 func _ready():
-	pass
-
-
+	heat = max_heat
+	emit_signal("heat_changed", heat)
+	
 func _on_Heat_body_entered(body):
 	if body is Player:
 		body.is_in_heat = true
 
-
 func _on_Heat_body_exited(body):
 	if body is Player:
 		body.is_in_heat = false
+
+func _cool_down(rain_drop):
+	heat -= 5
+	heat = max(0, heat)
+	emit_signal("heat_changed", heat)
+	
--- Campfire/Interface.gd
+++ Campfire/Interface.gd
@@ -3,8 +3,15 @@ extends Control
 signal heat_updated(value)
 
 func _ready():
-	var heat_node = node.get_node("Heat")
-	get_node("HeatMonitoring").initialize(heat_node.max_heat)
+#	var heat_node = get_node("Heat")
+#
+#	for node in get_tree().get_nodes_in_group("Campfire"):
+#		if node.name == "Campfire":
+#			heat_node = node.get_node("Heat")
+#			break
+#		get_node("HeatMonitoring").initialize(heat_node.max_heat)
+
+	$HeatMonitoring/HeatBar.value = 100
 	
 func _on_Heat_heat_changed(heat):
 	emit_signal("heat_updated", heat)
