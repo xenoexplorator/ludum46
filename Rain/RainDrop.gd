224671ede7617d6372ec72c34f5ef3b3266497b2
xenoexplorator <cedric.pinard@gmail.com>
2020-04-17 23:55:13 -0400

    Création d'une scène pour de la pluie (gameplay)

---
+++ Rain/RainDrop.gd
@@ -0,0 +1,14 @@
+extends KinematicBody2D
+
+export (float, -180, 180, 20) var angle := 0.0
+export (int) var speed := 50
+var _velocity : Vector2
+
+func _ready() -> void:
+	angle = deg2rad(angle)
+	_velocity = speed * Vector2(sin(angle), cos(angle))
+	print(_velocity)
+
+
+func _physics_process(delta: float) -> void:
+	move_and_collide(delta * _velocity)
