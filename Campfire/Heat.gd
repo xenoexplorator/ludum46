4bd706fdbca9a08d50fb1400f2f8d880a0a99d17
Francois Boivin <boivin.92@gmail.com>
2020-04-19 11:28:26 -0400

    fix broken particles campfire

--- Campfire/Heat.gd
+++ Campfire/Heat.gd
@@ -25,3 +25,5 @@ func _cool_down(rain_drop):
 		get_tree().call_group("hud", "update_campfire_health", heat)
 		emit_signal("heat_changed", heat)
 		rain_drop.turn_to_steam()
+		if heat == 0:
+			SceneTransitioner.transition_with_path("res://UI/Menu/MainMenu.tscn")
