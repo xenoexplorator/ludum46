71557fdb78108d72ec4f386eaff02b53b6fa418d
xenoexplorator <cedric.pinard@gmail.com>
2020-04-19 14:17:41 -0400

    Ménage de ressources inutilisées

--- Campfire/Interface.gd
+++
@@ -1,6 +0,0 @@
-extends Control
-
-signal heat_updated(value)
-	
-func _on_Campfire_heat_changed(heat):
-	emit_signal("heat_updated", heat)
--- Campfire/TextureProgress.gd
+++
@@ -1,5 +0,0 @@
-extends TextureProgress
-
-func _on_HeatMonitoring_max_changed(max_heat):
-	max_value = max_heat
-	
