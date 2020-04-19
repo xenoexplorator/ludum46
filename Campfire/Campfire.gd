264522eb2ea7fe5d8ad1307c691c748009302eab
Ellias, Mélanie <Melanie.Ellias@ia.ca>
2020-04-19 10:51:48 -0400

    degats sur les bûche

--- Campfire/Campfire.gd
+++ Campfire/Campfire.gd
@@ -1,5 +1,7 @@
 extends StaticBody2D
 
+class_name Campfire
+
 func take_rain(rain_drop):
 	$Heat._cool_down(rain_drop)
 
--- Rain/RainDrop.gd
+++ Rain/RainDrop.gd
@@ -10,19 +10,18 @@ func _ready() -> void:
 	angle = deg2rad(angle)
 	velocity = speed * Vector2(sin(angle), cos(angle))
 
-
 func _process(delta: float) -> void:
 	position += delta * velocity
 
-
 func _on_screen_exited() -> void:
 	queue_free()
-
 
 func _on_body_entered(body: Node) -> void:
 	if body is Player:
 		body.hurt()
 		turn_to_steam()
+	elif body is Campfire:
+		body.take_rain(self)
 	else:
 		queue_free()
 
